0bb7f67bdcdb9e39b6d9baaabccab5176d5ee98e
Jonah <duckwxrth@gmail.com>
2024-11-05 17:09:34 -0700

    chore: font cleanup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,5 @@
 import "./globals.css";
-
 import type { Metadata } from "next";
-import localFont from "next/font/local";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "The Christmas Party",
@@ -26,11 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className="antialiased">{children}</body>
     </html>
   );
 }
